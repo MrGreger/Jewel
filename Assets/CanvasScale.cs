03d41aa1f6ff3986745d3dbc42541037c61b520e
MrGreger <241000g@mail.ru>
2020-01-25 21:47:08 +0300

    Added canvas scaler

---
+++ Assets/CanvasScale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScale : MonoBehaviour
+{
+    private CanvasScaler _canvasScaler;
+    private Vector2 _screenResolution;
+
+    private void Start()
+    {
+        _canvasScaler = GetComponent<CanvasScaler>();
+        _screenResolution = new Vector2(Screen.width, Screen.height);
+        _canvasScaler.referenceResolution = _screenResolution;
+    }
+}
